8f982573974548267e1ef5a3e144f1a04c663338
Danny Boushka <boushka9@gmail.com>
2023-03-06 20:07:19 -0700

    Added psuedo-code outline

---
+++ routes/data-route.js
@@ -0,0 +1,10 @@
+// Dependencies for routing middleware
+const express = require('express');
+const router = express.Router();
+
+
+// GET request for rendering notes (get saved notes data to render?)
+
+// POST request to add new notes
+
+// DELETE request to remove individual notes by id w 'uuid' ?
