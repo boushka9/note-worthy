c6e1c0c011601190a0d2b88aab1618131ad9d021
Danny Boushka <boushka9@gmail.com>
2023-03-16 10:14:45 -0600

    Set up routes and helper functions with fs/path

--- routes/data-route.js
+++ routes/data-route.js
@@ -1,27 +1,10 @@
 // Dependencies for routing middleware
-const express = require('express');
-const router = express.Router();
-// Dependacy to parse saved notes data
-const saveNotes = require('../db/saveNotes');
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../lib/notesFunctions')
 
 
-// read the db.json file and return all saved notes as JSON.
-router.get('/notes', function (req, res) {
-    
-})
-  
 
- 
-// receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-router.post('/notes', (req, res) => {
-    
-})
-
-  
-// remove individual notes by id w 'uuid' 
-router.delete('/notes', (req, res) => {
-    
-})
   
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,28 +3,28 @@ const express = require('express');
 const path = require('path');
 
 // Connect server.js to route files
-const htmlRoute = require('./routes/html-route');
-const dataRoute = require('./routes/data-route');
+const routeDirectory = require('./routes/index');
+const apiRoute = require('./routes/data-route');
 
 // Initiate express server
 const app = express();
 
-
 // Set PORT to user environment for launching on heroor to default 3001
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 // Parse incoming urlencoded/JSON form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', htmlRoute);
-app.use('/api', dataRoute);
+// Use routes for homepage and notespage
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/api', apiRoute);
+app.use('/', routeDirectory);
 
-// GET Route for homepage (* as catch all)
-app.get('*', (req, res) =>
+
+// GET Route for homepage 
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-app.use(express.static('public'));
 
 // Listener to verify PORT
 app.listen(PORT, () => {
